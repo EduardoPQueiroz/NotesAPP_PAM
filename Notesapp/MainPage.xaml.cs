a2e9b660e70e9b9ea7b5cdde6d5d186e59445a02
EduardoPQueiroz <eduardopq4@gmail.com>
2024-03-07 11:36:27 -0300

    feat: adding project files

---
+++ Notesapp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Notesapp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = string.Empty; //ou "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
+        private void OnGreetClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            GreetLabel.Text = $"Ei {name} você mesmo, receba essa pedrada na sua cabeça!";
+        }
+    }
+    ///private void OnNameClicked()
+    ///{
+    ///
+    ///}
+
+}
