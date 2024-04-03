fab9c7ce1553e92efef5139221c1a04730949210
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2024-04-03 20:39:39 -0300

    feat - adding notes page to the project

--- Notesapp/MainPage.xaml.cs
+++ Notesapp/MainPage.xaml.cs
@@ -24,7 +24,7 @@
         private void OnGreetClicked(object sender, EventArgs e)
         {
             name = NameEntry.Text;
-            GreetLabel.Text = $"Ei {name} você mesmo, receba essa pedrada na sua cabeça!";
+            GreetLabel.Text = $"Ei {name} você mesmo, você é muitíssimo bem-vindo nesse app, tudo de bom pra você viu, não se preocupe fique a vontade, é um prazer tÊ-lo aqui, não tenha pressa para sair.";
         }
     }
     ///private void OnNameClicked()
---
+++ Notesapp/NotesPage.xaml.cs
@@ -0,0 +1,35 @@
+namespace Notesapp;
+
+public partial class NotesPage : ContentPage
+{
+    string path = Path.Combine(FileSystem.AppDataDirectory, "newfile.txt");
+    string text = "";
+
+    public NotesPage()
+    {
+        InitializeComponent();
+        if (File.Exists(path))
+        {
+            FileEditor.Text = File.ReadAllText(path);
+        }
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        //Pegar o texto escrito no editor de texto
+        //Armazenar esse texto em uma variável
+        text = FileEditor.Text;
+        //Criar um arquivo com esse conteúdo
+        File.WriteAllText(path, text);
+        DisplayAlert("Sucesso", "Arquivo salvo com sucesso", "Ok");
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            FileEditor.Text = "";
+        }
+    }
+}
